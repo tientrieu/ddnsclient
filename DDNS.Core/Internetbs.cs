2388b755d64f9a6dcb11993419ae254f94e467d5
Phung Tien Trieu <trieupt@coccoc.com>
2018-12-14 11:56:00 +0700

    Add projects

---
+++ DDNS.Core/Internetbs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+
+namespace DDNS.Core.ServiceProviders
+{
+    public class Internetbs : IServiceProvider
+    {
+        private string host;
+        private string user;
+        private string password;
+
+        public Internetbs(string host, string user, string password)
+        {
+            this.host = host;
+            this.user = user;
+            this.password = password;
+        }
+
+        public async void Update()
+        {
+            HttpClient client = new HttpClient();
+            string requestUrl = string.Format(
+                "https://dyndns.topdns.com/update?hostname={0}&username={1}&password={2}",
+                host, user, password);
+            var result = await client.GetStringAsync(requestUrl);
+            if (!result.StartsWith("good"))
+                throw new Exception(string.Format("Update DDNS fail. Server returned {0}", result));
+        }
+    }
+}
---
+++ DDNS.Core/ServiceProviders/IServiceProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDNS.Core.ServiceProviders
+{
+    public interface IServiceProvider
+    {
+        void Update();
+    }
+}
